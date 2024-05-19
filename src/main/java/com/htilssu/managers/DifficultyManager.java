3692815a9ba8d389e1365607c5b6488618584c2a
htilssu <tolashuu@gmail.com>
2024-05-20 00:46:44 +0700

    create frame

---
+++ src/main/java/com/htilssu/managers/DifficultyManager.java
@@ -0,0 +1,13 @@
+package com.htilssu.managers;
+
+import java.util.Map;
+
+public class DifficultyManager {
+    private static final int NORMAL = 1;
+    private static final int HARD = 2;
+    public static final Map<Integer, Integer> difficultyMap = Map.of(
+            NORMAL, 10,
+            HARD, 15
+    );
+    public static int difficulty = DifficultyManager.NORMAL;
+}
--- src/main/java/com/htilssu/settings/GameSetting.java
+++ src/main/java/com/htilssu/settings/GameSetting.java
@@ -1,13 +1,43 @@
 package com.htilssu.settings;
 
 public class GameSetting {
+    /**
+     * Kích thước của 1 TILE
+     */
     public static final int TILE_SIZE = 64;
+
+    /**
+     * Độ tăng trưởng của game (gấp mấy lần kích thước ban đầu)
+     */
     public static final int SCALE = 1;
+    /**
+     * Số lượng TILE trong chiều rộng
+     */
     public static final int TILE_IN_WIDTH = 20;
+    /**
+     * Số lượng TILE trong chiều cao
+     */
     public static final int TILE_IN_HEIGHT = 10;
+    /**
+     * Chiều rộng của cửa sổ game
+     * <p>
+     * Được tính theo công thức: TILE_SIZE * TILE_IN_WIDTH * SCALE
+     */
     public static final int WIDTH = TILE_SIZE * TILE_IN_WIDTH * SCALE;
+    /**
+     * Chiều cao của cửa sổ game
+     * <p>
+     * Được tính theo công thức: TILE_SIZE * TILE_IN_HEIGHT * SCALE
+     * </p>
+     */
     public static final int HEIGHT = TILE_SIZE * TILE_IN_HEIGHT * SCALE;
 
+    /**
+     * Số lượng frame mỗi giây
+     */
     public static int FPS = 60;
+    /**
+     * Số lượng tick mỗi giây
+     */
     public static int TPS = 120;
 }
