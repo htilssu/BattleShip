ab55209d015af51a9e31be35417901b62bd6750e
htilssu <tolashuu@gmail.com>
2024-06-23 20:37:05 +0700

    fix: Miss check can shoot before shoot

--- src/main/java/com/htilssu/entity/game/GamePlay.java
+++ src/main/java/com/htilssu/entity/game/GamePlay.java
@@ -14,7 +14,6 @@ import com.htilssu.multiplayer.Host;
 import com.htilssu.render.Renderable;
 import com.htilssu.setting.GameSetting;
 import com.htilssu.util.AssetUtils;
-import com.htilssu.util.GameLogger;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
@@ -137,7 +136,7 @@ public class GamePlay implements Renderable {
                 if (!board.isInside(position)) return;
                 Position pos = board.getBoardRowCol(position);
 
-                GameLogger.log("Player " + getCurrentPlayer().getName() + " shoot at " + pos);
+                if (!board.canShoot(pos)) return;
 
                 shoot(pos);
             }
@@ -227,6 +226,7 @@ public class GamePlay implements Renderable {
 
             playerBoard.removeShip(clickShip);
             setUpSprite = clickShip.getSprite();
+            direction = clickShip.getDirection();
             int count = shipInBoard.get(clickShip.getShipType());
             shipInBoard.replace(clickShip.getShipType(), ++count);
         }
